e4633f370854b676f6dc156f3417b51a71f1ce74
grubdragon <aryanagal98@gmail.com>
2017-09-30 02:10:16 +0530

    Subtitles version(hard) added, uses Tkinter and multithreading(INCOMPLETE)

--- subs.py
+++ subs.py
@@ -1,43 +1,61 @@
-import os,time
+import thread
+import Tkinter as tk
+import os
+import time
 import codecs
-def parset(line):
-    
-    hours = int(line.split(':')[0])
-    minutes = int(line.split(':')[1])
-    seconds = int(line.split(':')[2].split(',')[0])
-    milliseconds=int(line.split(':')[2].split(',')[1])
-    time=(hours*3600)+(minutes*60)+(seconds)+(milliseconds/1000)
-    print(time)
+
+root = tk.Tk()
+prevTime=0
+delayBefore=[]
+persistenceTime=[]
+lyricList=[]
+currentLyric=0
+
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def time_in_seconds(line):
+    line = line.replace(',',':')
+    hours,minutes,seconds,milliseconds = [int(n) for n in line.split(":")]
+    time=(hours*3600)+(minutes*60)+(seconds)+(milliseconds/1000.0)
     return(time)
-
-
-#srt_file = open("lyrics.srt",'rU')
-with codecs.open("lyrics.srt", "rU", encoding="utf-8-sig") as srt_file:
-    s=srt_file.readlines()
-    for i in range(len(s)):
-        if(s[i].split('\n')[0][0].isdigit()):
-
-            if(len(s[i].split('\n')[0])>2):
-                if (s[i].split('\n')[0][2].isdigit()):
-                    continue
-                else:
-                    print(str(5))
-                    s1=s[i].split(' --> ')[1].split('\n')[0]
-                    print(s1)
-                    s2=s[i].split(' --> ')[0]
-                    print(s2)
-                    delay=parset(s1)-parset(s2)
-                    print(str(delay))
-                    time.sleep(delay)
-        else:
-            print(s[i].split('\n')[0].split('\n')[0])
-                  
-
         
-
-# i=0
-# s=(srt_file.readlines())
-# for i in range()
-
-# print (srt_file.readlines())
-srt_file.close()
+def add_lyric_to_screen():
+    x = currentLyric
+    currentLyric+=1
+    lyric = lyricList[x]
+    persistence = persistenceTime[x]
+    l = tk.Label(root,text=lyric)
+    l.pack()
+    time.sleep(persistence)
+    l.destroy()
+    
+with codecs.open("lyrics.srt", "rU", encoding="utf-8-sig") as srt_file:
+    line1 = srt_file.readline()
+    while line1:
+        if RepresentsInt(line1):
+            time = srt_file.readline()
+            lyric=""
+            line2 = srt_file.readline()
+            
+            while line2:
+                if line2.strip() == "":
+                    break
+                lyric=lyric+line2+"\n"
+                line2 = srt_file.readline()
+            
+            begin,sep,end=time.strip().split()
+            delayBefore.append(time_in_seconds(begin)-prevTime)
+            prevTime = time_in_seconds(begin)
+            persistenceTime.append(time_in_seconds(end)-time_in_seconds(begin))
+            lyricList.append(lyric)
+        line1 = srt_file.readline()
+        
+root.mainloop()
+for t in delayBefore:
+    time.sleep(t)
+    thread.start_new_thread(add_lyric_to_screen, ())
